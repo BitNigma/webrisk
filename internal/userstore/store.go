1a09056446f22deeb1871247b90389c15112a395
Freddy Nigma <jackkakos@gmail.com>
2024-07-26 08:13:16 +0300

    data

--- internal/userstore/store.go
+++ internal/userstore/store.go
@@ -15,18 +15,20 @@ type Store struct {
 func NewStore() *Store {
 
 	ctx := context.Background()
-	cfg, err := config.NewConfig()
+	_, err := config.NewConfig()
 	if err != nil {
 		log.Println("can't get config", err)
 		return nil
 	}
 
-	conn, err := NewPG(ctx, cfg.URL)
+	pgurl := "postgres://postgres:Flints1488@ec2-13-51-242-14.eu-north-1.compute.amazonaws.com:5432/users?sslmode=disable"
+
+	conn, err := NewPG(ctx, pgurl)
 	if err != nil {
 		log.Println("can't initialize Postgres", err)
 	}
 	if err = conn.db.Ping(ctx); err != nil {
-		log.Println("can't connect to c2c DataBase", err)
+		log.Println("can't connect to DataBase", err)
 	}
 
 	return &Store{
